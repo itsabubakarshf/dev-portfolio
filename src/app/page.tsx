bcba5a52f2ff4d3dd0f2f7b627e3b51dcaff85f7
itsabubakarshf <hi@abubakarshf.dev>
2023-05-14 18:04:22 +0500

    basic seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import Image from 'next/image'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <center>
-     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Website is under development &nbsp;
-          <code className="font-mono font-bold">~ AbubakarShf</code>
-        </p>
-      </div>
-     </center>
+      <Head>
+        <title>AbubakarShf - Website Under Development</title>
+        <meta name="description" content="AbubakarShf's personal website is currently under development." />
+        <meta name="author" content="AbubakarShf" />
+      </Head>
+      <center>
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Website is under development &nbsp;
+            <code className="font-mono font-bold">~ AbubakarShf</code>
+          </p>
+        </div>
+      </center>
     </main>
-  )
+  );
 }
